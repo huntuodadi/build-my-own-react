5a03b8bd1c9521f3efbc2ff63be8fe842c1fe5b5
zy.zhang <490733118@q.com>
2020-07-19 19:57:38 +0800

    add requestIdcallback

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // const element = <h1 title="foo">Hello</h1>
+import { createElement } from './react';
 import ReactDom from './react-dom';
-const element = {
-  type: 'h1',
-  props: {
-    title: 'foo',
-    children: 'Hello'
-  },
-};
-console.log('run');
+const element = createElement(
+  'div',
+  { id: 'foo' },
+  createElement('button', { id: 'btn1' }, 'click me' ),
+  createElement('a', null, 'link'),
+)
+console.log('run:', element);
 const container = document.getElementById('root');
 ReactDom.render(element, container);
--- react-dom.js
+++ react-dom.js
@@ -1,16 +1,17 @@
 const ReactDom = new Object();
-ReactDom.render = function(element, container) {
+function render(element, container) {
   const { type, props } = element;
   const { children, ...otherProps } = props;
-  console.log('otherProps:', otherProps);
-  const node = document.createElement(type)
-  for(const attr in otherProps) {
-    console.log('attr:', attr);
-    node[attr] = otherProps[attr];
-  }
-  const text = document.createTextNode('');
-  text.nodeValue = children;
-  node.appendChild(text);
-  container.append(node);
+  const dom = type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(type);
+  Object.keys(otherProps).forEach(propName => {
+    console.log('propName:', propName);
+    dom[propName] = otherProps[propName];
+  })
+  children.forEach((child) => {
+    render(child, dom)
+  })
+  container.appendChild(dom);
 }
+
+ReactDom.render = render;
 export default ReactDom;
