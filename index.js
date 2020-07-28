7cf7a2ca15c5726fbe72108d5844e25d97b3e0c3
zy.zhang <490733118@q.com>
2020-07-28 23:53:24 +0800

    fdf

--- index.js
+++ index.js
@@ -308,16 +308,13 @@ const Didact = {
 }
 
 /** @jsx Didact.createElement */
-// function Counter() {
-//   const [state, setState] = Didact.useState(1)
-//   return (
-//     Didact.createElement("h1", {
-//       onClick: () => setState(c => c + 1)
-//     }, "Count: ", state)
-//   )
-// }
-const element = Didact.createElement("h1", {
-  onClick: () => setState(c => c + 1)
-}, "Count: ")
+function Counter() {
+  const [state, setState] = Didact.useState(1);
+  return /*#__PURE__*/Didact.createElement("h1", {
+    onClick: () => setState(c => c + 1)
+  }, "Count: ", state);
+}
+
+const element = /*#__PURE__*/Didact.createElement(Counter, null);
 const container = document.getElementById("root")
 Didact.render(element, container)
